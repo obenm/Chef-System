8eae587da26cd9990d816de89784a65c83c653d6
obenm <octavioben@MBP-de-Octavio.lan>
2017-04-15 13:55:44 -0600

    Initial Commit
    Include creation of multiple app, as:
    
    — Mobile App with Ionic 2
    — Admin App with Ionic 2
    — Server with NodeJS

---
+++ Server/server.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+    app = express(),
+    bodyParser  = require("body-parser"),
+    methodOverride = require("method-override");
+    mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
+});
